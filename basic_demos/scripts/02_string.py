1beb80f43f8afda3dd6fb6327053a431dcfe5970
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2024-05-20 16:39:44 +0530

    day#1 update#3

---
+++ basic_demos/scripts/02_string.py
@@ -0,0 +1,6 @@
+str="India is Great"
+
+print(f'|{str.upper()}|')
+print(f'|{str.ljust(50)}|')
+print(f'|{str.center(50)}|')
+print(f'|{str.rjust(50)}|')
---
+++ basic_demos/scripts/04_input.py
@@ -0,0 +1,4 @@
+x= int(input('enter first number:'))
+y= input('enter second number:')
+
+print(x + int(y))
